877b818d57f98ecaa0d0c73850e554f03719fb81
Andrew Musielak <46854857+Drewl919@users.noreply.github.com>
2023-09-25 16:36:57 -0500

    worked in class

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,14 +1,51 @@
 import hashlib
 
-def hash(p):
-    m = p.encode() # Concerts text to byte code
-    h = hashlib.sha256(m).hexdigest() # converts to binary representation, then calls the hex version of the output
-    return h
+def hash256(p):
+    m = p.encode()  # Concerts text to byte code (UTF-8) - Unicode representation
+    # tion
+    h = hashlib.sha256(m)  # converts to binary representation
+    return h.hexdigest()  # calls the hex version of the output
     # return hashlib.sha256(p.encode("utf-8")).hexdigest()
-
-def main() :
+def hashShaw3_256(p):
+    m = p.encode()
+    h = hashlib.sha3_256(m)
+    return h.hexdigest()
+def hashShake256(p):
+    m = p.encode()
+    h = hashlib.shake_256(m)
+    return h.hexdigest(32)
+def main():
     w = "helloworld"
     print(w)
-    print(hash(w))
+    print(f"SHA-256   {hash256(w)}")
+    print(f"SHA3-256  {hashShaw3_256(w)}")
+    print(f"SHAKE-256 {hashShake256(w)}")
+main()
+# print(hashlib.algorithms_available)
+# print('helloworld'.encode()) # print of encoded value
+# print(hashlib.sha256('helloworld'.encode())) # Gets the address of the hash object, not the actual hex
+# print(hashlib.sha256('helloworld'.encode()).hexdigest()) # Returns the hex (hash) value [digest]
+
+def main():
+    w = 'helloworld'
+    w2 = w.encode()
+
+    print(f"   SHA-256 {hashlib.sha256(w2).hexdigest()}")
+    print(f"  SHA3-256 {hashlib.sha3_256(w2).hexdigest()}")
+    print(f" SHAKE-256 {hashlib.shake_256(w2).hexdigest(32)}")
+    w3 = w2 + 'apple'.encode()
+    print(f"cShakeLike {hashlib.shake_256(w3).hexdigest(32)}") # cShake
+
+    print()
+    print("------- Tuple -------")
+
+    acct = '5001'
+    amt = '50'
+    inputString = str(len(acct)) + acct + str(len(amt)) + amt
+    print(f"SHAKE-256 {hashlib.shake_256(inputString.encode()).hexdigest(32)}")
+
+    # print(hashlib.algorithms_available)
 
 main()
+
+
